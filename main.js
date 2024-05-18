e9c8f661bbc18e8404bf71d3987a46d491bd74b3
Bruna Eduarda Kohler <134429539+BRUNAEDUARDAKOHLER@users.noreply.github.com>
2024-05-17 23:03:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -74,5 +74,5 @@ function classificaSenha(tamanhoAlfabeto) {
         forcaSenha.classList.add("fraca");
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "Um coputador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir esta senha.";
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir esta senha.";
 }
